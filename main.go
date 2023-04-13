c07882086c6035dc1279c61dbfefa5be6de3efb2
imsil <ims.ilham@gmail.com>
2023-04-13 13:40:26 +0700

    struktur-project

--- main.go
+++ main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator/v10"
+	"github.com/vsualzm/gin-cek/handler"
 )
 
 func main() {
@@ -14,77 +11,15 @@ func main() {
 	router := gin.Default()
 
 	//2
-	router.GET("/", rootHandler)
 
-	router.GET("/hello", helloHandler)
-
-	router.GET("/books/:id/:title", booksHandler)
-
-	router.GET("/query", queryHandler)
-
-	router.POST("/books", postBooksHandler)
+	v1 := router.Group("/v1")
+	v1.GET("/", handler.RootHandler)
+	v1.GET("/hello", handler.HelloHandler)
+	v1.GET("/books/:id/:title", handler.BooksHandler)
+	v1.GET("/query", handler.QueryHandler)
+	v1.POST("/books", handler.PostBooksHandler)
 
 	//3
 	router.Run()
-
-}
-
-func rootHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"name": "ilham maulana",
-		"bio":  "Seorang programmer",
-	})
-}
-
-func helloHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"data": "helohelo",
-		"sub":  "bau kemem",
-	})
-}
-
-func booksHandler(c *gin.Context) {
-	id := c.Param("id")
-	title := c.Param("title")
-	c.JSON(http.StatusOK, gin.H{"id": id, "title": title})
-}
-
-func queryHandler(c *gin.Context) {
-	title := c.Query("title")
-	price := c.Query("price")
-
-	c.JSON(http.StatusOK, gin.H{"title": title, "price": price})
-}
-
-type BookInput struct {
-	Title string      `json:"title" binding:"required"`
-	Price interface{} `json:"price" binding:"required,number"`
-}
-
-func postBooksHandler(c *gin.Context) {
-	// title dan price
-	// menangkap dari struct
-
-	var BookInput BookInput
-
-	err := c.ShouldBindJSON(&BookInput)
-	if err != nil {
-
-		errorMessages := []string{}
-		for _, e := range err.(validator.ValidationErrors) {
-			errorMessage := fmt.Sprintf("error on field %s, condition:%s", e.Field(), e.ActualTag())
-			errorMessages = append(errorMessages, errorMessage)
-		}
-
-		c.JSON(http.StatusBadRequest, gin.H{
-			"errors": errorMessages,
-		})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"title": BookInput.Title,
-		"price": BookInput.Price,
-	})
 
 }

bf25d2f00f27947d840db3055e2e595a52f5aa99
imsil <ims.ilham@gmail.com>
2023-04-13 11:56:25 +0700

    Handler-GET-POS-path-query

---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	//1
+	router := gin.Default()
+
+	//2
+	router.GET("/", rootHandler)
+
+	router.GET("/hello", helloHandler)
+
+	router.GET("/books/:id/:title", booksHandler)
+
+	router.GET("/query", queryHandler)
+
+	router.POST("/books", postBooksHandler)
+
+	//3
+	router.Run()
+
+}
+
+func rootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"name": "ilham maulana",
+		"bio":  "Seorang programmer",
+	})
+}
+
+func helloHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"data": "helohelo",
+		"sub":  "bau kemem",
+	})
+}
+
+func booksHandler(c *gin.Context) {
+	id := c.Param("id")
+	title := c.Param("title")
+	c.JSON(http.StatusOK, gin.H{"id": id, "title": title})
+}
+
+func queryHandler(c *gin.Context) {
+	title := c.Query("title")
+	price := c.Query("price")
+
+	c.JSON(http.StatusOK, gin.H{"title": title, "price": price})
+}
+
+type BookInput struct {
+	Title    string
+	Price    int
+	SubTitle string `json:"sub_title"`
+}
+
+func postBooksHandler(c *gin.Context) {
+	// title dan price
+
+	// menangkap dari struct
+
+	var BookInput BookInput
+
+	err := c.ShouldBindJSON(&BookInput)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"title":     BookInput.Title,
+		"price":     BookInput.Price,
+		"sub_title": BookInput.SubTitle,
+	})
+
+}
